0449047d9e74720db00bbe52e3b3ba98356018b7
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-10-01 09:00:41 +0300

    edit data

--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -10,36 +10,36 @@ namespace LibraryManagementSystem
         {
 
             using LibraryDbcontext db =new LibraryDbcontext();    
-            bool data = false;
+            //bool data = false;
 
 
-            try
-            {
-                // data = SeedingData.seeding<Member>("Files/Members.json", db);
-                // data = SeedingData.seeding<Category>("Files/Categories.json", db);
-                // data = SeedingData.seeding<Author>("Files/Authors.json", db);
-                data = SeedingData.seeding<Book>("Files/Books.json", db);
+            //try
+            //{
+            //    // data = SeedingData.seeding<Member>("Files/Members.json", db);
+            //    // data = SeedingData.seeding<Category>("Files/Categories.json", db);
+            //    // data = SeedingData.seeding<Author>("Files/Authors.json", db);
+            //    data = SeedingData.seeding<Book>("Files/Books.json", db);
 
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(" File not found: " + ex.FileName);
-            }
-            catch (Exception ex)
-            {
+            //}
+            //catch (FileNotFoundException ex)
+            //{
+            //    Console.WriteLine(" File not found: " + ex.FileName);
+            //}
+            //catch (Exception ex)
+            //{
 
-                Console.WriteLine($"Error while seeding Airlines: {ex.Message}");
-
-
-
-            }
+            //    Console.WriteLine($"Error while seeding Airlines: {ex.Message}");
 
 
-            if (data)
-            {
 
-                Console.WriteLine(" Airlines data saved successfully.");
-            }
+            //}
+
+
+            //if (data)
+            //{
+
+            //    Console.WriteLine(" Airlines data saved successfully.");
+            //}
           
         }
     }
